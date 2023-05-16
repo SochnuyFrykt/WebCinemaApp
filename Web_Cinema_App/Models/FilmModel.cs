a7df053bd92a4b5729520e7673aebc2660168452
SochnuyFrykt <serebrenikovdanil@gmail.com>
2023-05-16 17:56:17 +0400

    Add film model

--- Web_Cinema_App/Models/FilmModel.cs
+++ Web_Cinema_App/Models/FilmModel.cs
@@ -1,10 +1,20 @@
-﻿namespace Web_Cinema_App.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Web_Cinema_App.Models
 {
     public class FilmModel
     {
+        [System.ComponentModel.DataAnnotations.Key]
+        [Column("id")]
         public int Id { get; set; }
+
+        [Column("name_film")]
         public string Name { get; set; }
+
+        [Column("description")]
         public string Description { get; set; }
+
+        [Column("id_genre")]
         public string Genre { get; set; }
     }
 }
