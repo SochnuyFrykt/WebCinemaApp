df9120a57bcf4a180d6d5a297cfa49dc100aed3d
SochnuyFrykt <serebrenikovdanil@gmail.com>
2023-05-16 19:04:11 +0400

    Add ticket model

---
+++ Web_Cinema_App/Models/TicketModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Web_Cinema_App.Models
+{
+    public class TicketModel
+    {
+        [System.ComponentModel.DataAnnotations.Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("id_session")]
+        public int IdSession { get; set; }
+
+        [Column("id_place")]
+        public int IdPlace { get; set; }
+
+        [Column("status")]
+        public bool Status { get; set; }
+
+        [Column("price")]
+        public int Price { get; set; }
+    }
+}
