916787bda4d29c5571d873ffee8824ae8d3cace2
SochnuyFrykt <serebrenikovdanil@gmail.com>
2023-05-16 19:01:04 +0400

    Add session model

---
+++ Web_Cinema_App/Models/SessionModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Web_Cinema_App.Models
+{
+    public class SessionModel
+    {
+        [System.ComponentModel.DataAnnotations.Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("date")]
+        public DateTime Date { get; set; }
+
+        [Column("time")]
+        public DateTime Time { get; set; }
+
+        [Column("id_film")]
+        public int IdFilm { get; set; }
+    }
+}
