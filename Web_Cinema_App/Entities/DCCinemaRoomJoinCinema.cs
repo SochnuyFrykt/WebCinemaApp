22a5e6ee9f9bfe36bdae0b99b57910728307108e
SochnuyFrykt <serebrenikovdanil@gmail.com>
2023-06-01 00:53:53 +0400

    Add DataContextJoin

--- Web_Cinema_App/Entities/DCCinemaRoomJoinCinema.cs
+++ Web_Cinema_App/Entities/DCCinemaRoomJoinCinema.cs
@@ -1,6 +1,24 @@
-﻿namespace Web_Cinema_App.Entities
+﻿using Microsoft.EntityFrameworkCore;
+using Web_Cinema_App.Models;
+
+namespace Web_Cinema_App.Entities
 {
-    public class DCCinemaRoomJoinCinema
+    public class DCCinemaRoomJoinCinema : DbContext
     {
+        private string ConnectionPath =
+           "Host=localhost;" +
+           "Port=5432;" +
+           "Username=postgres;" +
+           "Password=root;" +
+           "Database=Cinema_DB;";
+
+        public DbSet<CinemaModel> Cinema { get; set; }
+        public DbSet<CinemaRoomModel> CinemaRoom { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(ConnectionPath);
+            base.OnConfiguring(optionsBuilder);
+        }
     }
 }
